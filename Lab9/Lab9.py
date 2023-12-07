d483a30d02a062d9f4b40e545584fe5f0ab00a89
G00385433@atu.ie <g00385433@atu.ie>
2023-12-07 14:13:29 +0000

    further work done
    added while loop to add circles over edges in img Harris
    
    now onto Shi Tomasi Algorithm for edge detection - about to make deep copy to protect image

--- Lab9/Lab9.py
+++ Lab9/Lab9.py
@@ -1,7 +1,7 @@
 # Libraries
 import cv2
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import copy
 
 # Read Image and set to Grayscale
@@ -11,9 +11,22 @@ img_gray = cv2.cvtColor(cv2.imread('ATU1.jpg'), cv2.COLOR_BGR2GRAY)
 dst = cv2.cornerHarris(img_gray, 2, 3, 0.04) 
 
 # Deep copy
-dc = copy.deepcopy(img_gray)
+imgHarris = copy.deepcopy(img_gray)
 
-print(dst)
+# Plot cv2 circles on imgHarris
+threshold = 0.5; #number between 0 and 1
+for i in range(len(dst)):
+    for j in range(len(dst[i])):
+        if dst[i][j] > (threshold*dst.max()):
+            cv2.circle(imgHarris,(j,i),3,( 70, 235, 52 ),-1)
 
-cv2.imshow("Image",img_gray)
+
+#Plot imgHarris
+cv2.imshow("Image",imgHarris)
 cv2.waitKey(0)
+
+# Shi Tomasi Algorithm
+corners = cv2.goodFeaturesToTrack(img_gray,10,0.01,10)
+
+print(corners)
+
